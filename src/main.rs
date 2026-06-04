ada42e79d95105fd2ecea9010613b1df853792b0
Morshed <golammorshed004@gmail.com>
2026-06-04 11:57:25 +0600

    Make a Tic‑Tac‑Toe Game in rust

---
+++ src/main.rs
@@ -0,0 +1,125 @@
+use std::io;
+
+const PLAYER_X: char = 'X';
+const PLAYER_O: char = 'O';
+const BOARD_SIZE: usize = 3;
+
+type Board = [[char; BOARD_SIZE]; BOARD_SIZE];
+
+fn initialize_board() -> Board {
+    [[' '; BOARD_SIZE]; BOARD_SIZE]
+}
+
+fn print_board(board: &Board) {
+    for row in board {
+        for cell in row {
+            print!("{} ", cell);
+        }
+        println!();
+    }
+}
+
+fn get_player_move(current_player: char, board: &Board) -> (usize, usize) {
+    loop {
+        println!("Player {} input (row col)", current_player);
+
+        let mut user_input = String::new();
+        io::stdin()
+            .read_line(&mut user_input)
+            .expect("Failed to read input");
+
+        let coordinates: Vec<usize> = user_input
+            .split_whitespace()
+            .map(|s| s.parse::<usize>())
+            .collect::<Result<Vec<_>, _>>()
+            .unwrap_or(vec![]);
+
+        if coordinates.len() == 2 {
+            let (row, col) = (coordinates[0], coordinates[1]);
+
+            if row < BOARD_SIZE && col < BOARD_SIZE && board[row][col] == ' ' {
+                return (row, col);
+            }
+        }
+
+        println!("Invalid input! Try again.");
+    }
+}
+
+fn check_winner(current_player: char, board: &Board) -> bool {
+    for row in 0..BOARD_SIZE {
+        if board[row][0] == current_player
+            && board[row][1] == current_player
+            && board[row][2] == current_player
+        {
+            return true;
+        }
+    }
+    for col in 0..BOARD_SIZE {
+        if board[0][col] == current_player
+            && board[1][col] == current_player
+            && board[2][col] == current_player
+        {
+            return true;
+        }
+    }
+
+    if (board[0][0] == current_player
+        && board[1][1] == current_player
+        && board[2][2] == current_player)
+        || (board[0][2] == current_player
+            && board[1][1] == current_player
+            && board[2][0] == current_player)
+    {
+        return true;
+    }
+
+    return false;
+}
+
+fn check_draw(board: &Board) -> bool {
+    for row in board {
+        for cell in row {
+            if *cell == ' ' {
+                return false;
+            }
+        }
+    }
+    true
+}
+
+fn play_game() {
+    let mut board = initialize_board();
+    let mut current_player = PLAYER_X;
+
+    loop {
+        println!("Current Board:");
+        print_board(&board);
+
+        let (row, col) = get_player_move(current_player, &board);
+        board[row][col] = current_player;
+
+        if check_winner(current_player, &board) {
+            print_board(&board);
+            println!("{} is the winner!", current_player);
+            break;
+        }
+
+        if check_draw(&board) {
+            print_board(&board);
+            println!("The game is a draw!");
+            break;
+        }
+
+        current_player = if current_player == PLAYER_X {
+            PLAYER_O
+        } else {
+            PLAYER_X
+        };
+    }
+}
+
+fn main() {
+    println!("Welcome to Tic‑Tac‑Toe Game!");
+    play_game();
+}
